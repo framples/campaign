57fd3a08b95b4d5e6695e6a8c122451eda3196f0
unknown <pwfcoding@gmail.com>
2019-09-27 00:18:45 -0500

    getting inquirer running

--- app.js
+++ app.js
@@ -1,22 +1,72 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 let inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
+  port     : "3306",
   password : 'Blitzcrank13',
   database : 'dungeons_db'
 });
  
-connection.connect();
+
  
-connection.query(function (error, results, fields) {
-  if (error) throw error;
-  console.log("Error");
+connection.connect(function (err, res) {
+  if (err) throw err;
+  askName();
 });
+
+function askName() {
+    inquirer
+    .prompt({ 
+            name: "playername",
+            type: "input",
+            message: "What is your name?"
+            
+        })
+        .then(function(answer){
+            let query = "SELECT player FROM playerinfo WHERE ?";
+            connection.query(query, {player: answer.player}, function(err,res) {
+                if (err) throw err;
+                for (let i = 0; i < res.length; i++) {
+                    console.log("Name: " + res[i].player);
+                }
+                
+            });
+        });
+    }
+        
+        
+        
+        
+        
+        /*{
+            name: "charactername",
+            message: "What is your character's full name?",
+            type: "input"
+
+        }, 
+        {
+            name: "class",
+            message: "Please select the class of your character.",
+            type: "list",
+            choices: [ "Barbarian", "Bard", "Blood Hunter", "Blood Mage", "Chef", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard" ]        
+        },
+        {
+            name: "race",
+            message: "What class is your character?",
+            type: "list",
+            choices: ["Dwarf", "Goblin", "Halfing", "Human", "Minotaur", "Orc", "Tiefling", "Troll", "Vampire"]
+
+        }
+    ]);  console.log("Hello " + user.name);
+
+   
+
+
+
  
 
 
 
 
-
-connection.end();
+*/
