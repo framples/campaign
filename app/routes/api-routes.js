284f731ed099beb74bfba4b4ad5431a3cea9f8e7
unknown <pwfcoding@gmail.com>
2019-10-11 17:07:49 -0500

    succesfully connected to server - api-routes will be constant updating

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -2,3 +2,6 @@ let Players = require("../models/players.js");
 let Characters = require("../models/characters.js");
 let Feedback = require("../models/feedback.js");
 
+module.exports = function(app) {
+
+};
