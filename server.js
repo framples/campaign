6625d806528f6a8db45d77b4e3981e570d6637cd
unknown <pwfcoding@gmail.com>
2019-10-21 21:18:40 -0500

    not sure what these changes are tbh

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 let express = require("express");
-let bodyParser = require("body-parser");
-let cookieParser = require("cookie-parser");
 let session = require("express-session");
-let morgan = require("morgan");
 
 let app = express();
 let PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-
-
-/////////////////CODE I'M NOT FAMILIAR WITH//////////////////////////////////////
-
-app.use(morgan("dev"));
-
-app.use(session({
-    key: "user_sid",
-    secret: "somerandomstuffs",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
+app.use(session( {
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
 }));
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
 
 
 app.use(express.static("app/public"));
