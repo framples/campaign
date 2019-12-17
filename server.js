a982ceb99f31339062b9a344838b818b86511cba
PeterFairley <pwfcoding@gmail.com>
2019-12-17 17:14:34 -0600

    heroku6

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(
     })
 )
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://uncleframpton:happyholidays@apex-khhhx.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 
